ca5fd0b591a14cdd1071b3a7c8e554b9cc75a04d
JEON <jeon34986@naver.com>
2022-02-04 11:00:42 +0900

    Initial commit

---
+++ src/main/java/com/example/basic/deliveryServiceApi/DeliveryServiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.basic.deliveryServiceApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryServiceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/basic/deliveryServiceApi/DeliveryServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.basic.deliveryServiceApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryServiceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
